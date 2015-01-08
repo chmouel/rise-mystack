5fb8321e4ed0a5dd0e519c4b5ec16041711838f8
Chmouel Boudjnah <chmouel@enovance.com>
2015-01-08 20:14:17 +0100

    s/apt/bootstrap/

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,8 +72,8 @@ cat <<EOF>~/.shell/hosts/${HOSTNAME%%.*}.sh
 # hostColor="yellow"
 # userColor="white"
 
-alias inst="sudo apt-get -y install"
-alias remove="sudo apt-get -y remove"
+alias inst="sudo yum -y install"
+alias remove="sudo yum -y remove"
 alias g="ack-grep --color-match 'bold blue'"
 alias -g SP="|curl -F 'sprunge=<-' http://sprunge.us"
 
